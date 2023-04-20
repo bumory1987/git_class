cbdc6a52cb5321f0e8617f80b07b5c87a3c2fc89
bum <bumory1987@gmail.com>
2023-04-20 13:51:36 +0900

    space one

---
+++ funcTest.py
@@ -0,0 +1,34 @@
+
+
+def conflictCheckOne(index, func):
+    func(index)
+    return 10 
+
+
+
+def conflictCheckTwo(index, func):
+    print("no no no ")
+    print("on my god")
+
+
+
+
+
+
+
+
+def conflictCheckThree(index, func):
+    func(index)
+    print("conflict")
+    return 10 
+
+
+def conflictCheckFour(index):
+    print("show me")
+
+def conflictCheckFour(index, func):
+    func(index)
+    print("conflict")
+    return 10 
+
+
