df5eb386550429cf49d9c3d1a9aab56af04e5370
bum <bumory1987@gmail.com>
2023-04-21 09:58:03 +0900

    finalize

--- conflict_test.py
+++ conflict_test.py
@@ -1,14 +1,5 @@
 def conflictOne(simple, value ,test):
     print("hello")
-<<<<<<< HEAD
-
-def conflictOne(simple, value):
-    print("oh my god")
-    
-def conflictThree(index, func, good):
-    func(good(index))
-    return 10 
-=======
     
     
 def conflictThree(index, func):
@@ -22,4 +13,3 @@ def conflictOne(simple, value ):
     print(simple(value))
     print("_________")
     
->>>>>>> main
