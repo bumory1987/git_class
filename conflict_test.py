c87779e394ee648d88c2b205ab04e4a3cb9e800a
bum <bumory1987@gmail.com>
2023-04-21 09:15:35 +0900

    conflict rebase test

---
+++ conflict_test.py
@@ -0,0 +1,10 @@
+def conflictOne(simple, value ,test):
+    print("hello")
+    
+    
+def conflictThree(index, func):
+    func(index)    
+    return 10 
+
+def conflictOne(simple, value):
+    print("oh my god")
